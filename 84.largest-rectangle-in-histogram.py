be472e3bf5afc746eb4d1a9383688d651952501c
sgrtye <sgrtye@gmail.com>
2023-11-10 01:33:00 +0000

    two more stack questions

---
+++ 84.largest-rectangle-in-histogram.py
@@ -0,0 +1,63 @@
+# @lcpr-before-debug-begin
+from python3problem84 import *
+from typing import *
+# @lcpr-before-debug-end
+
+#
+# @lc app=leetcode id=84 lang=python3
+# @lcpr version=30108
+#
+# [84] Largest Rectangle in Histogram
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        max_area = 0
+        
+        for i, h in enumerate(heights):
+            if not stack:
+                stack.append((h, i))
+            else:
+                if h > stack[-1][0]:
+                    stack.append((h, i))
+                elif h == stack[-1][0]:
+                    continue
+                else:
+                    while stack and h < stack[-1][0]:
+                        tmp = stack.pop()
+                        max_area = max(max_area, (i - tmp[1]) * tmp[0])
+                    stack.append((h, tmp[1]))
+        
+        for h, i in stack:
+            max_area = max(max_area, (len(heights) - i) * h)
+        
+        return max_area
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [2,1,5,6,2,3]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [2,4]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [2,1,2]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [0,0]\n
+# @lcpr case=end
+
+#
+
---
+++ 853.car-fleet.py
@@ -0,0 +1,57 @@
+# @lcpr-before-debug-begin
+from python3problem853 import *
+from typing import *
+# @lcpr-before-debug-end
+
+#
+# @lc app=leetcode id=853 lang=python3
+# @lcpr version=30108
+#
+# [853] Car Fleet
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        info = list(zip(position, speed))
+        info.sort(key = lambda x: x[0], reverse = True)
+
+        count = 0
+        previous_time = None
+        for p, s in info:
+            required_time = (target - p) / s
+            if not previous_time:
+                previous_time = required_time
+                count += 1
+            elif previous_time < required_time:
+                previous_time = required_time
+                count += 1
+            else:
+                # In this case, the current cat catches to the front car
+                pass
+        
+        return count
+
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# 12\n[10,8,0,5,3]\n[2,4,1,1,3]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 10\n[3]\n[3]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 100\n[0,2,4]\n[4,2,1]\n
+# @lcpr case=end
+
+#
+
