21e629f4f5174a061067c2fe3fef6b3f84959962
sgrtye <sgrtye@gmail.com>
2023-11-13 23:40:13 +0000

    koko

--- 875.koko-eating-bananas.py
+++ 875.koko-eating-bananas.py
@@ -13,18 +13,18 @@ class Solution:
         res = r
 
         while l <= r:
-            k = (l + r) // 2
+            mid = l + ((r - l) // 2)
 
             totalTime = 0
 
             for p in piles:
-                totalTime += (p // k) + 1 if p % k != 0 else p // k
+                totalTime += p // mid if p % mid == 0 else (p // mid) + 1
                 
             if totalTime <= h:
-                res = k
-                r = k - 1
+                res = mid
+                r = mid - 1
             else:
-                l = k + 1
+                l = mid + 1
 
         return res
 
